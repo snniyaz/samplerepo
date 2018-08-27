8585e08057caf1193ea08b739d02ad2d80e60b29
Niyazuddin Syed <sniyazuddin@nationallife.com>
2018-08-27 14:40:11 +0530

    HelloWorkld file added

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to the Remote repo");
+            Console.WriteLine("This third change");
+            Console.Write("Press any key to cont...");
+            
+            Console.ReadKey(true);
+        }
+    }
+}
